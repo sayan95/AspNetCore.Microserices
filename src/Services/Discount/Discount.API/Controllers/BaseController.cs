6a2439961cd5bdee59ed5d772090affcabc5c2f6
sayan95 <sayanmallik1995@gmail.com>
2021-11-10 21:48:08 +0530

    Discount api onboarding

---
+++ src/Services/Discount/Discount.API/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Discount.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+
+    }
+}
---
+++ src/Services/Discount/Discount.API/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Discount.API
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            var scope = host.Services.CreateScope();
+
+            var services = scope.ServiceProvider;
+
+            var logger = services.GetRequiredService<ILogger<Program>>();
+
+            logger.LogInformation("Happy onboarding to Discount.API");
+            await host.RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
