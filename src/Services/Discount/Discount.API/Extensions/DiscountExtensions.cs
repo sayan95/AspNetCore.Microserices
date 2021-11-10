29830d619ec907df3cc762359f98d8db7f4eb15b
sayan95 <sayanmallik1995@gmail.com>
2021-11-11 01:18:58 +0530

    DAL and Entities implemented

---
+++ src/Services/Discount/Discount.API/Extensions/DiscountExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Discount.API.Repositories.Classes;
+using Discount.API.Repositories.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Discount.API.Extensions
+{
+    public static class DiscountExtensions
+    {
+        public static IServiceCollection DiscountServices(this IServiceCollection services) {
+            services.AddScoped<IDiscountRepository, DiscountRepository>();
+            return services;
+        }
+    }
+}
