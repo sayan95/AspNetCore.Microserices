928614fe83256f3d2934a110e29bfe0170f62985
sayan95 <sayanmallik1995@gmail.com>
2021-11-11 17:31:18 +0530

    Discount db seeders implemented

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,7 +1,6 @@
+using Discount.API.Extensions;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 
 namespace Discount.API
@@ -11,13 +10,7 @@ namespace Discount.API
         public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
-            var scope = host.Services.CreateScope();
-
-            var services = scope.ServiceProvider;
-
-            var logger = services.GetRequiredService<ILogger<Program>>();
-
-            logger.LogInformation("Happy onboarding to Discount.API");
+            host.MigrateDatabase<Program>();
             await host.RunAsync();
         }
 
